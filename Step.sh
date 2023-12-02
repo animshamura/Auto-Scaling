ec82ffaca264e90cdac7665604bbb03136492308
Shamura Ahmad Anim <47661479+animshamura@users.noreply.github.com>
2023-12-02 21:17:20 +0600

    Create Step.sh

---
+++ Step.sh
@@ -0,0 +1,5 @@
+# Step 1 : Creating VPC, Subnet, Route Table and Inter Gateway and associate them with each other.  <br/>
+# Step 2 : Creating Target Group and Load Balancer but Target Group doesn't contain any ec2 instance.  <br/>
+# Step 3 : Creating Launch Template with certain security group.  <br/>
+# Step 4 : Creating auto Scalling Group. <br/>
+# Step 5 : Checking the set up! 
